378f9505555ab6c46e23e74f01295f1ba8fd8287
Lefro1 <65259917+Lefro1@users.noreply.github.com>
2024-05-12 02:44:27 -0700

    Tested and updated script for all directories

--- src/helpers/clean_up_duplicate_files.py
+++ src/helpers/clean_up_duplicate_files.py
@@ -2,8 +2,13 @@ import os
 
 
 def remove_duplicate_files(root_dir):
+    counter = 0
+
     for root, dirs, files in os.walk(root_dir):
         for filename in files:
+            counter += 1
+            if counter % 100000 == 0:
+                print(f"-------------Checking: {root}/{filename}")
             if filename.endswith('.png'):
                 basename, ext = os.path.splitext(filename)
                 chapter_number, sub_number = basename.split('_')
@@ -11,9 +16,9 @@ def remove_duplicate_files(root_dir):
                     chapter_number = chapter_number.lstrip('0')
                     if f"{chapter_number}_{sub_number}.png" in files:
                         os.remove(os.path.join(root, filename))
-                        print(f"Deleted: {filename}")
+                        print(f"Deleted: {root}/{filename}")
 
 
 # Replace 'root_directory' with the path to your root directory containing all the folders with images
-root_directory = 'E:\Manga\WebtoonDownloader\The Lone Necromancer'
+root_directory = 'E:\Manga\WebtoonDownloader'
 remove_duplicate_files(root_directory)
